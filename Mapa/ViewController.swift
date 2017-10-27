18bde87f0e8e7fb9d5fe8074ac3eb4cc80ead31a
Kamil <kamil@Kamils-iMac.local>
2017-10-27 15:13:17 +0200

    added working location manager and privileges

--- Mapa/ViewController.swift
+++ Mapa/ViewController.swift
@@ -9,20 +9,32 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     @IBOutlet weak var clearAllButton: UIButton!
-
-    let locationManager = CLLocationManager()
+    
+    let locationManager: CLLocationManager = CLLocationManager()
+    var paused: Bool = true
+    var isRunning: Bool = false
+    var markers: [MapMarker] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         stopButton.isEnabled = false
-        centerMapOnUserLocation()
+        mapView.delegate = self
+        mapView.showsScale = true
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,29 +44,48 @@ class ViewController: UIViewController {
     @IBAction func onStartPress(_ sender: UIButton) {
         startButton.isEnabled = false
         stopButton.isEnabled = true
+        locationManager.startUpdatingLocation()
+        paused = false
+        mapView.showsUserLocation = true
+        if(isRunning) {
+            return
+        }
+        isRunning = true
+        onUpdate()
     }
     @IBAction func onStopPress(_ sender: UIButton) {
         startButton.isEnabled = true
         stopButton.isEnabled = false
+        locationManager.stopUpdatingLocation()
+        paused = true
+        mapView.showsUserLocation = false
     }
     @IBAction func onClearAllPress(_ sender: UIButton) {
-        
+        mapView.removeAnnotations(markers)
+        markers.removeAll()
     }
     
-    func centerMapOnUserLocation() {
-        print("update")
-        var speed: CLLocationSpeed = (locationManager.location?.speed)!
-        if speed < 0 {
-            speed = 0
+    func onUpdate() {
+        if paused {
+            isRunning = false
+            return
         }
-        let regionRadius: CLLocationDistance = CLLocationDistance((450*speed)+500)
-        let coordinateRegion = MKCoordinateRegionMakeWithDistance(mapView.userLocation.coordinate,
-                                                                  regionRadius, regionRadius)
-        mapView.setRegion(coordinateRegion, animated: true)
-        
-        let when = DispatchTime.now() + 5
-        DispatchQueue.main.asyncAfter(deadline: when) {
-            self.centerMapOnUserLocation()
+        if let location = locationManager.location {
+            if let speed = location.speed as? CLLocationSpeed {
+                let regionRadius: CLLocationDistance = CLLocationDistance((25*speed)+150)
+                let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,                                                                  regionRadius, regionRadius)
+                addressLabel.text = "\(location.coordinate.latitude), \(location.coordinate.longitude)"
+                mapView.setRegion(coordinateRegion, animated: true)
+                let marker = MapMarker(coordinate: location.coordinate)
+                mapView.addAnnotation(marker)
+                markers.append(marker)
+                
+            }
+            
+            let when = DispatchTime.now() + 5
+            DispatchQueue.main.asyncAfter(deadline: when) {
+                self.onUpdate()
+            }
         }
     }
     
